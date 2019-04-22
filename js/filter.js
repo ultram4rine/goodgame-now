1fb542a5919f2dc8510ad1256f844d64bdf6ca10
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-22 21:52:26 +0400

    Added live search

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    $(document).on("input",function(ev){
+        if ($(ev.target).val().length > 0) {
+            var name = $(ev.target).val();
+            $('a div[name]').hide();
+            $('[name^=' + name + ']').show();
+        } else {
+            $('a div[name]').show();
+        }
+    });
+
     $('#search').focus(function(){
         $('#filter').addClass('focused');
     });
