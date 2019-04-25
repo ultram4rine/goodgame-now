593d37d55cfccf22504a6db14830c8b0e2bca76c
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-25 23:20:01 +0400

    Getting elements moved out from onload function

--- js/fav.js
+++ js/fav.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
         btn.parent().children().css("background-color", "#1C2644");
         btn.css("background-color", "#52709C");
 
+        var topbar = document.getElementById('topbar');
+        topbar.innerHTML = '';
+        var content = document.getElementById('content');
+        content.innerHTML = '';
+
         var x = new XMLHttpRequest();
         x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
         x.send()
         x.onload = function (){
             if (x.status == 403) {
-                var topbar = document.getElementById('topbar');
-                topbar.innerHTML = '';
-    
-                var content = document.getElementById('content');
-                content.innerHTML = '';
                 content.innerHTML += '<div id="unauth">Вы не вошли<br><button id="login">Вход</button></div>';
             } else {
                 var topbar = document.getElementById('topbar');
@@ -27,13 +27,9 @@ $(document).ready(function(){
                     $("#filter").removeClass("focused");
                 })
 
-                var content = document.getElementById('content');
-                content.innerHTML = '';
-
                 var answer = JSON.parse(x.responseText);
                 for (var i = 0; i < answer.length; i++) {
                     if (answer[i].status == true) {
-                        console.log(answer[i].title)
                         content.innerHTML += '<a target="_blank" rel="noopener noreferrer" href="' + answer[i].link + '#autoplay"><div id="' + i + '" name="' + answer[i].streamer.nickname + '" class="stream"><img src="http:' + answer[i].preview + '"><div class="text"><div class="channame">' + answer[i].streamer.nickname + '</div><div class="description" title="' + answer[i].game + ' - ' + answer[i].viewers + ' зрителей"><div class="name">' + answer[i].game + ' -&nbsp;</div><div class="viewers">' + answer[i].viewers + ' зрителей</div></div><div title="' + answer[i].title + '" class="title">' + answer[i].title + '</div></div></div></a>';
                     }
                 }
