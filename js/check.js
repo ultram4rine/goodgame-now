ab3b4cfae472f09c7706eaf28009fe63d25fdcc2
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-26 21:58:28 +0400

    Moved notifications to interval

--- js/check.js
+++ js/check.js
@@ -1,19 +1,26 @@
-var x = new XMLHttpRequest();
-x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
-x.send()
-x.onload = function (){
-    if (x.status == 200) {
-        var streams = '';
-        var answer = JSON.parse(x.responseText);
-        for (var i = 0; i < answer.length; i++) {
-            if (answer[i].status == true) {
-                streams += answer[i].streamer.nickname;
+var time = localStorage.getItem("ggnowtimeupdate");
+var itime = parseInt(time);
+var interval = itime * 60 * 1000;
+
+setInterval(function(){
+    var x = new XMLHttpRequest();
+    x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
+    x.send()
+    x.onload = function (){
+        if (x.status == 200) {
+            var streams = '';
+            var answer = JSON.parse(x.responseText);
+            for (var i = 0; i < answer.length; i++) {
+                if (answer[i].status == true) {
+                    streams += answer[i].streamer.nickname;
+                }
+            }
+            if (streams != '') {
+                var notification = new Notification(
+                    streams,
+                    {icon: "../icons/64.png"}
+                );  
             }
         }
-        var notification = new Notification(
-            streams,
-            {icon: "../icons/64.png"}
-        );
-        notification.show();
-    }
-};
+    };
+}, interval)
