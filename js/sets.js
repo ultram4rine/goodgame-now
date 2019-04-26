161c66f00cac40e071a87b3e16efbd110cf871f6
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-26 21:23:28 +0400

    Added autoupdate time slider in settings

--- js/sets.js
+++ js/sets.js
@@ -9,12 +9,23 @@ $(document).ready(function(){
 
         var content = document.getElementById('content');
         content.innerHTML = '';
-        var value = localStorage.getItem("ggnowstyle");
-        if (value == "twitchnow") {
-            content.innerHTML += '<div id="style" class="sets">Стиль вкладок<br><input type="radio" id="twitchnow" name="style" value="twitchnow" checked><label for="twitchnow">Twitch Now style</label><input type="radio" id="goodgame" name="style" value="goodgame"><label for="goodgame">GoodGame style</label></div>'
-        } else if (value == "goodgame") {
-            content.innerHTML += '<div id="style" class="sets">Стиль вкладок<br><input type="radio" id="twitchnow" name="style" value="twitchnow"><label for="twitchnow">Twitch Now style</label><input type="radio" id="goodgame" name="style" value="goodgame" checked><label for="goodgame">GoodGame style</label></div>'
+        var style = localStorage.getItem("ggnowstyle");
+        var time = localStorage.getItem("ggnowtimeupdate");
+        if (style == "twitchnow") {
+            content.innerHTML += '<div id="style" class="sets">Стиль вкладок<br><input type="radio" id="twitchnow" name="style" value="twitchnow" checked><label for="twitchnow">Twitch Now style</label><input type="radio" id="goodgame" name="style" value="goodgame"><label for="goodgame">GoodGame style</label></div><div id="time" class="sets">Автообновление через<br><input type="range" id="slide" min="1" max="60" step="1" value="5""><span id="val"></span></div>'
+        } else if (style == "goodgame") {
+            content.innerHTML += '<div id="style" class="sets">Стиль вкладок<br><input type="radio" id="twitchnow" name="style" value="twitchnow"><label for="twitchnow">Twitch Now style</label><input type="radio" id="goodgame" name="style" value="goodgame" checked><label for="goodgame">GoodGame style</label></div><div id="time" class="sets">Автообновление через<br><input type="range" id="slide" min="1" max="60" step="1" value="5""><span id="val"></span></div>'
         }
+        
+        if (time == null) {
+            time = '5';
+        }
+        $('#slide').val(time)
+        $('#val').html(time + ' мин.');
+        $(document).on('input', '#slide', function() {
+            $('#val').html($(this).val() + ' мин.');
+            localStorage.setItem("ggnowtimeupdate", $(this).val());
+        });
 
         $('input[type=radio][name=style]').change(function() {
             if (this.value == 'twitchnow') {
