0cccb30c28c6d8d284334e6af2f31423bba23c43
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-27 23:38:36 +0400

    Added default time update

--- js/style.js
+++ js/style.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-    var value = localStorage.getItem("ggnowstyle");
-    if (value == null) {
-        value = "twitchnow";
-        localStorage.setItem("ggnowstyle", value);
+    var style = localStorage.getItem("ggnowstyle");
+    var time = localStorage.getItem("ggnowtimeupdate");
+        
+    if (time == null) {
+        time = '5';
+        localStorage.setItem("ggnowtimeupdate", time);
     }
-    if (value == "twitchnow") {
+    if (style == null) {
+        style = "twitchnow";
+        localStorage.setItem("ggnowstyle", style);
+    }
+    if (style == "twitchnow") {
         $('#style[rel=stylesheet]').attr('href', '../css/twnow.css');
-    } else if (value == "goodgame") {
+    } else if (style == "goodgame") {
         $('#style[rel=stylesheet]').attr('href', '../css/gg.css');
     }
 });
