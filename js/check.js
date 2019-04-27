72bcbf04c1802d9b1c7d1c3c933dfc264c7f9c38
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-27 23:37:57 +0400

    Notifications for firefox

--- js/check.js
+++ js/check.js
@@ -1,26 +1,57 @@
 var time = localStorage.getItem("ggnowtimeupdate");
 var itime = parseInt(time);
 var interval = itime * 60 * 1000;
+var streams = '';
 
-setInterval(function(){
+var x = new XMLHttpRequest();
+var streamsprev = streams;
+x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
+x.send()
+x.onload = function (){
+    if (x.status == 200) {
+        var answer = JSON.parse(x.responseText);
+        var k = 0;
+        for (var i = 0; i < answer.length; i++) {
+            if (answer[i].status == true) {
+                streams += answer[i].streamer.nickname;
+                k++;
+                chrome.browserAction.setBadgeText({text: "" + k});
+            }
+        }
+        if (streams != '' && streams != streamsprev) {
+            browser.notifications.create({
+                "type": "basic",
+                "iconUrl": browser.extension.getURL("../icons/64.png"),
+                "title": "GoodGame Now",
+                "message": streams
+            })
+        }
+    }
+};
+
+setInterval(function notif(){
     var x = new XMLHttpRequest();
+    var streamsprev = streams;
+    streams = '';
     x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
     x.send()
     x.onload = function (){
         if (x.status == 200) {
-            var streams = '';
             var answer = JSON.parse(x.responseText);
+            var k = 0;
             for (var i = 0; i < answer.length; i++) {
                 if (answer[i].status == true) {
                     streams += answer[i].streamer.nickname;
+                    k++;
+                    chrome.browserAction.setBadgeText({text: "" + k});
                 }
             }
-            if (streams != '') {
+            if (streams != '' && streams != streamsprev) {
                 var notification = new Notification(
                     streams,
                     {icon: "../icons/64.png"}
-                );  
+                )
             }
         }
     };
-}, interval)
+}, interval);
