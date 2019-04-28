4473f638b34c1750a0f732dd5aa663a3b8c9dc1f
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-28 16:29:32 +0400

    Moved settings initialize to background, style set to another script

---
+++ js/setsInit.js
@@ -0,0 +1,11 @@
+var style = localStorage.getItem("ggnowstyle");
+var time = localStorage.getItem("ggnowtimeupdate");
+        
+if (time == null) {
+    time = '5';
+    localStorage.setItem("ggnowtimeupdate", time);
+}
+if (style == null) {
+    style = "twitchnow";
+    localStorage.setItem("ggnowstyle", style);
+}
--- js/styleSet.js
+++ js/styleSet.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
     var style = localStorage.getItem("ggnowstyle");
-    var time = localStorage.getItem("ggnowtimeupdate");
-        
-    if (time == null) {
-        time = '5';
-        localStorage.setItem("ggnowtimeupdate", time);
-    }
-    if (style == null) {
-        style = "twitchnow";
-        localStorage.setItem("ggnowstyle", style);
-    }
     if (style == "twitchnow") {
         $('#style[rel=stylesheet]').attr('href', '../css/twnow.css');
     } else if (style == "goodgame") {
