3391d0d584554b3c84e09129cdd31c6fc2fb8a1d
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-04-28 16:21:21 +0400

    Completed notifications

--- js/check.js
+++ js/check.js
@@ -1,38 +1,13 @@
-var time = localStorage.getItem("ggnowtimeupdate");
-var itime = parseInt(time);
-var interval = itime * 60 * 1000;
-var streams = '';
+var streams = [];
 
-var x = new XMLHttpRequest();
-var streamsprev = streams;
-x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
-x.send()
-x.onload = function (){
-    if (x.status == 200) {
-        var answer = JSON.parse(x.responseText);
-        var k = 0;
-        for (var i = 0; i < answer.length; i++) {
-            if (answer[i].status == true) {
-                streams += answer[i].streamer.nickname;
-                k++;
-                chrome.browserAction.setBadgeText({text: "" + k});
-            }
-        }
-        if (streams != '' && streams != streamsprev) {
-            chrome.notifications.create({
-                "type": "basic",
-                "iconUrl": chrome.extension.getURL("../icons/64.png"),
-                "title": "GoodGame Now",
-                "message": streams
-            })
-        }
-    }
-};
-
-setInterval(function notif(){
+(function notif() {
+    var time = localStorage.getItem("ggnowtimeupdate");
+    var itime = parseInt(time);
+    var interval = itime * 60 * 1000;
     var x = new XMLHttpRequest();
     var streamsprev = streams;
-    streams = '';
+    streams = [];
+
     x.open("GET", "https://goodgame.ru/api/4/favorites/", true);
     x.send()
     x.onload = function (){
@@ -41,19 +16,34 @@ setInterval(function notif(){
             var k = 0;
             for (var i = 0; i < answer.length; i++) {
                 if (answer[i].status == true) {
-                    streams += answer[i].streamer.nickname;
+                    streams[i] = answer[i].streamer.nickname;
                     k++;
                     chrome.browserAction.setBadgeText({text: "" + k});
                 }
             }
-            if (streams != '' && streams != streamsprev) {
+            diffRes = diff(streams, streamsprev);
+            if (streams.length != 0 && !compare(streams, streamsprev) && !streamsprev.some(r=> diffRes.indexOf(r) >= 0)) {
+                var newstreams = '';
+                for (var i = 0; i < diffRes.length; i++) {
+                    if (diffRes[i] != undefined) {
+                        newstreams += diffRes[i] + '\n';
+                    }
+                }
                 chrome.notifications.create({
                     "type": "basic",
                     "iconUrl": chrome.extension.getURL("../icons/64.png"),
                     "title": "GoodGame Now",
-                    "message": streams
+                    "message": newstreams
                 })
             }
         }
     };
-}, interval);
+    setTimeout(notif, interval);
+})();
+
+diff = function (a1, a2) {
+    return a1.filter(i=>!a2.includes(i)).concat(a2.filter(i=>!a1.includes(i)))
+}
+compare = function (a1, a2) {
+    return a1.length == a2.length && a1.every((v,i)=>v === a2[i])
+}
