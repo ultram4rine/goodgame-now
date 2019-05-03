91460f1a6b161a6f3782ba871d36c3b501ed7dcc
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-05-03 18:14:31 +0400

    Rewrited getting top streams to v4 API

--- js/top.js
+++ js/top.js
@@ -1,88 +1,82 @@
 $(document).ready(function(){
-    $("#top").click(function(){
-        var btn = $(this);
-        btn.parent().children().css("background-color", "#1C2644");
-        btn.css("background-color", "#52709C");
-    
-        var x = new XMLHttpRequest();
-        x.open("GET", "https://goodgame.ru/api/getggchannels", true);
-        x.onload = function (){
-            var topbar = document.getElementById('topbar');
-            topbar.innerHTML = '';
-            topbar.innerHTML += '<div id="filter"><span class="icosearch"></span><input id="search" type="text"></div>';
-
-            $("#search").focusin(function(){
-                $("#filter").addClass("focused");
-            })
-            $("#search").focusout(function(){
-                $("#filter").removeClass("focused");
-            })
-
-            var content = document.getElementById('content');
-            content.innerHTML = '';
-            var answer = JSON.parse(x.responseText);
-            var count = Object.keys(answer.channels);
-            for (var i = 0; i < 30; i++) {
-                content.innerHTML += '<a target="_blank" rel="noopener noreferrer" href="https://goodgame.ru/channel/' + answer.channels[i].key + '/#autoplay"><div id="' + i + '" name="' + answer.channels[i].key + '" class="stream"><img src="' + answer.channels[i].thumb + '"><div class="text"><div class="channame">' + answer.channels[i].key + '</div><div class="description" title="' + answer.channels[i].games.title + ' - ' + answer.channels[i].viewers + ' зрителей"><div class="name">' + answer.channels[i].games.title + ' -&nbsp;</div><div class="viewers">' + answer.channels[i].viewers + ' зрителей</div></div><div title="' + answer.channels[i].title + '" class="title">' + answer.channels[i].title + '</div></div></div></a>';
-
-                var iStr = '' + i;
-
-                if ($('link[href="../css/gg.css"]').attr('href') == '../css/gg.css') {
-                    elem = $('#' + iStr + ' .viewers');
-                    var str = elem.text();
-                    if (str.search(' зрителей') != -1) {
-                        elem.html(elem.html().replace(" зрителей", ""));
-                    }
-                }
-            }
-            i = 30;
-            iStr = '' + i - 1;
-            var lastRow = document.getElementById(iStr);
-            var busy;
-            content.addEventListener('scroll', function() {
-                checkLoading();
-            });
-    
-            function Loader(){
-                if(busy) return;
-                busy = true;
-                content.classList.add("load");
-                window.setTimeout(onSuccess, 300);
-            }
-    
-            function onSuccess() {
-                content.classList.remove("load");
-                var Cont = "";
-                for (var j = 0; j < 30; j++) {
-                    if (i < count.length) {
-                        content.innerHTML += '<a target="_blank" rel="noopener noreferrer" href="https://goodgame.ru/channel/' + answer.channels[i].key + '/#autoplay"><div id="' + i + '" name="' + answer.channels[i].key + '" class="stream"><img src="' + answer.channels[i].thumb + '"><div class="text"><div class="channame">' + answer.channels[i].key + '</div><div class="description" title="' + answer.channels[i].games.title + ' - ' + answer.channels[i].viewers + ' зрителей"><div class="name">' + answer.channels[i].games.title + ' -&nbsp;</div><div class="viewers">' + answer.channels[i].viewers + ' зрителей</div></div><div title="' + answer.channels[i].title + '" class="title">' + answer.channels[i].title + '</div></div></div></a>';
-                        i++;
-                        var iStr = '' + i - 1;
-                        if ($('link[href="../css/gg.css"]').attr('href') == '../css/gg.css') {
-                            elem = $('#' + iStr + ' .viewers');
-                            var str = elem.text();
-                            if (str.search(' зрителей') != -1) {
-                                elem.html(elem.html().replace(" зрителей", ""));
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                lastRow.insertAdjacentHTML("afterEnd", Cont);
-                lastRow = document.getElementById(iStr);
-                busy = false;
-            }
-                
-            function checkLoading() {
-                var sheight = document.getElementById('content').scrollHeight;
-                var top = document.getElementById('content').scrollTop;
-                var cheight = document.getElementById('content').clientHeight;
-                if (sheight - top === cheight) {
-                    Loader();
-                }
-            }
-        }
-    x.send(null);
+    var pagecount = 0;
+    chrome.runtime.sendMessage({
+        msg: "page"
+    }, function(response) {
+        pagecount = response;
+        var i = 1;
+        tops(i, pagecount);
     });
 });
+
+function tops(i, pagecount){
+    prev = $('#prev');
+    next = $('#next');
+    if (i == 1) {
+        prev.off("click");
+        prev.css('cursor', 'default');
+        prev.css('color', '#ACACAC');
+        prev.css('background-color', '#2F323D');
+
+        next.off("click");
+        next.css('cursor', 'pointer');
+        next.css('color', '#73ADFF');
+        next.css('background-color', '#21273B');
+        next.click(function(){
+            i++;
+            $('#topstreams').html('');
+            tops(i, pagecount);
+        });
+    } else if (i == pagecount) {
+        next.off("click");
+        next.css('cursor', 'default');
+        next.css('color', '#ACACAC');
+        next.css('background-color', '#2F323D');
+
+        prev.off("click");
+        prev.css('cursor', 'pointer');
+        prev.css('color', '#73ADFF');
+        prev.css('background-color', '#21273B');
+        prev.click(function(){
+            i--;
+            $('#topstreams').html('');
+            tops(i, pagecount);
+        });
+    } else {
+        next.off("click");
+        next.css('cursor', 'pointer');
+        next.css('color', '#73ADFF');
+        next.css('background-color', '#21273B');
+        next.click(function(){
+            i++;
+            $('#topstreams').html('');
+            tops(i, pagecount);
+        });
+
+        prev.off("click");
+        prev.css('cursor', 'pointer');
+        prev.css('color', '#73ADFF');
+        prev.css('background-color', '#21273B');
+        prev.click(function(){
+            i--;
+            $('#topstreams').html('');
+            tops(i, pagecount);
+        });
+    }
+    var x = new XMLHttpRequest();
+    x.open("GET", "https://goodgame.ru/api/4/stream?page=" + i + "/", true);
+    x.send()
+    x.onload = function (){
+        if (x.status == 200) {
+            var content = '';
+            wrap = $('#topstreams');
+            var answertop = JSON.parse(x.responseText);
+            for (var i = 0; i < answertop.streams.length; i++) {
+                if (answertop.streams[i].status != false) {
+                    content += '<a target="_blank" rel="noopener noreferrer" href="' + answertop.streams[i].link + '#autoplay"><div id="' + i + '" name="' + answertop.streams[i].streamer + '" class="stream"><img src="http:' + answertop.streams[i].preview + '"><div class="text"><div class="channame">' + answertop.streams[i].streamer + '</div><div class="description"><div class="viewers">' + answertop.streams[i].viewers + ' зрителей</div></div><div title="' + answertop.streams[i].title + '" class="title">' + answertop.streams[i].title + '</div></div></div></a>'
+                }
+            }
+            wrap.html(content)
+        }
+    }
+}
