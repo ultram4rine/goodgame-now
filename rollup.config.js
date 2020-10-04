c1b42dcd97716e08d5716ebe57b02ff39cbde937
ultram4rine <ultramarine730@gmail.com>
2020-10-04 15:28:09 +0400

    added babel and node-resolve rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
 export default {
   input: "src/main.js",
   output: {
-    file: "bundle.js",
-    format: "es",
+    dir: "build",
+    format: "esm",
   },
+  plugins: [babel({ babelHelpers: "bundled" }), nodeResolve()],
 };
