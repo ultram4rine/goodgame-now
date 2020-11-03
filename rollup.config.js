f8071a7a2275628e91f4210ede8444504e13fa27
ultram4rine <ultramarine730@gmail.com>
2020-11-03 20:49:18 +0400

    new rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,38 @@
-import babel from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import minifyHTML from "rollup-plugin-minify-html-literals";
+import copy from "rollup-plugin-copy";
+import {
+  chromeExtension,
+  simpleReloader,
+} from "rollup-plugin-chrome-extension";
 
-export default {
-  input: "src/main.js",
-  output: {
-    dir: "build",
-    format: "esm",
-  },
-  plugins: [babel({ babelHelpers: "bundled" }), nodeResolve()],
+const copyConfig = {
+  targets: [
+    { src: "node_modules/@webcomponents", dest: "dist/node_modules" },
+    { src: "images", dest: "dist" },
+    { src: "data", dest: "dist" },
+    { src: "index.html", dest: "dist" },
+  ],
 };
+
+const config = {
+  input: "src/manifest.json",
+  output: {
+    dir: "dist",
+    format: "es",
+  },
+  plugins: [
+    chromeExtension(),
+    simpleReloader(),
+    minifyHTML(),
+    copy(copyConfig),
+    resolve(),
+  ],
+};
+
+if (process.env.NODE_ENV !== "development") {
+  config.plugins.push(terser());
+}
+
+export default config;
