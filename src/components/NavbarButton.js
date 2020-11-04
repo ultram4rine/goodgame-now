7f73837d3d07256d70b827b257222a4f97f5c9db
ultram4rine <ultramarine730@gmail.com>
2020-11-04 14:01:08 +0400

    fixed styles definition, used fontawesome icons

--- src/components/NavbarButton.js
+++ src/components/NavbarButton.js
@@ -3,34 +3,36 @@ import { LitElement, html, css, customElement, property } from "lit-element";
 @customElement("navbar-button")
 export class NavbarButton extends LitElement {
   @property({ type: String })
-  class = "";
+  icon = "";
 
-  static styles = css`
-    .navbar-button {
-      width: 40px;
-      height: 40px;
-      float: left;
-      display: table-cell;
-      cursor: pointer;
-      text-align: center;
-      vertical-align: middle;
-      line-height: 40px;
-      font-family: icomoon;
-      font-size: 22px;
-      background-color: #1c2644;
-      color: white;
-      transition: 0.2s;
-      box-shadow: inset -5px 0 5px -4px black;
-    }
-    .navbar-button:hover {
-      background-color: #52709c !important;
-    }
-  `;
+  static get styles() {
+    return css`
+      .navbar-button {
+        width: 40px;
+        height: 40px;
+        float: left;
+        display: table-cell;
+        cursor: pointer;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 40px;
+        font-family: icomoon;
+        font-size: 22px;
+        background-color: #1c2644;
+        color: white;
+        transition: 0.2s;
+        box-shadow: inset -5px 0 5px -4px black;
+      }
+      .navbar-button:hover {
+        background-color: #52709c !important;
+      }
+    `;
+  }
 
   render() {
     return html`
       <div class="navbar-button">
-        <span class="${this.class}"></span>
+        <i class="fas fa-${icon}"></i>
       </div>
     `;
   }
