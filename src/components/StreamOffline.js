d1c9e7cf536b9a0ea0f1a7ef92a8a529c7d097ef
ultram4rine <ultramarine730@gmail.com>
2020-11-04 14:00:14 +0400

    fixed styles definitions

--- src/components/StreamOffline.js
+++ src/components/StreamOffline.js
@@ -11,40 +11,42 @@ export class StreamOffline extends LitElement {
   @property({ type: String })
   start = "";
 
-  static styles = css`
-    .inactive {
-      cursor: pointer;
-      width: 100%;
-      height: 55px;
-      line-height: 55px;
-      border-bottom: 1px solid black;
-      text-decoration: none;
-      font-family: -apple-system, Open Sans, sans-serif;
-      font-size: 14px;
-      color: #63a0fa;
-    }
-    .inactive:hover {
-      background-color: #2e3a58;
-    }
-    .inactive img {
-      width: 35px;
-      height: 35px;
-      float: left;
-      margin-top: 10px;
-      margin-left: 10px;
-      border: 1px solid rgb(62, 77, 113);
-    }
-    .inactive .nickname {
-      width: auto;
-      margin-left: 5px;
-      float: left;
-    }
-    .inactive .start {
-      margin-right: 10px;
-      float: right;
-      color: #43b85b;
-    }
-  `;
+  static get styles() {
+    return css`
+      .inactive {
+        cursor: pointer;
+        width: 100%;
+        height: 55px;
+        line-height: 55px;
+        border-bottom: 1px solid black;
+        text-decoration: none;
+        font-family: -apple-system, Open Sans, sans-serif;
+        font-size: 14px;
+        color: #63a0fa;
+      }
+      .inactive:hover {
+        background-color: #2e3a58;
+      }
+      .inactive img {
+        width: 35px;
+        height: 35px;
+        float: left;
+        margin-top: 10px;
+        margin-left: 10px;
+        border: 1px solid rgb(62, 77, 113);
+      }
+      .inactive .nickname {
+        width: auto;
+        margin-left: 5px;
+        float: left;
+      }
+      .inactive .start {
+        margin-right: 10px;
+        float: right;
+        color: #43b85b;
+      }
+    `;
+  }
 
   render() {
     return html`
