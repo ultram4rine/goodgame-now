491d5bbb82e4e8d7b30c746f8565905985e2867a
ultram4rine <ultramarine730@gmail.com>
2022-05-25 02:51:59 +0400

    show login button if not authenticated

--- src/pages/Favorites.svelte
+++ src/pages/Favorites.svelte
@@ -1,13 +1,56 @@
 <script>
   import { getFavoriteStreams } from "../api/streams";
 
+  import { authenticated } from "../stores/auth";
+
   import Stream from "../components/Stream.svelte";
 
-  let streamsPromise = getFavoriteStreams();
+  $: streamsPromise = $authenticated
+    ? getFavoriteStreams()
+    : Promise.resolve([]);
 </script>
 
-{#await streamsPromise then streams}
-  {#each streams as stream (stream.id)}
-    <Stream {stream} />
-  {/each}
-{/await}
+{#if !$authenticated}
+  <div id="unauth">
+    Вы не вошли<br />
+    <a target="_blank" rel="noopener noreferrer" href="https://goodgame.ru">
+      <button id="login">Вход</button>
+    </a>
+  </div>
+{:else}
+  {#await streamsPromise then streams}
+    {#each streams as stream (stream.id)}
+      <Stream {stream} />
+    {/each}
+  {/await}
+{/if}
+
+<style>
+  #unauth {
+    position: absolute;
+    font-family: -apple-system, Open Sans, sans-serif;
+    color: white;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  #unauth button {
+    width: 70px;
+    height: 35px;
+    margin-top: 3px;
+    cursor: pointer;
+    border: 0;
+    border-radius: 2px;
+    font-family: -apple-system, Open Sans, sans-serif;
+    font-size: 14px;
+    background-color: #394466;
+    color: #73adff;
+  }
+  #unauth button:hover {
+    color: #458eff;
+  }
+  #unauth button:active {
+    outline: none;
+  }
+</style>
