cb553a96ba4a5d9952b69a82028e2d012d030991
ultram4rine <ultramarine730@gmail.com>
2022-05-25 02:41:30 +0400

    remove try/catch from api calls

--- src/api/streams.js
+++ src/api/streams.js
@@ -5,40 +5,28 @@ import { apiEndpoint } from ".";
 import { loading } from "../stores/loading";
 
 export const getFavoriteStreams = async () => {
-  try {
-    loading.set("loading");
-    const resp = await axios.get(`${apiEndpoint}/favorites`);
-    loading.set("loaded");
-    return resp.data;
-  } catch (err) {
-    console.log(err);
-  }
+  loading.set("loading");
+  const resp = await axios.get(`${apiEndpoint}/favorites`);
+  loading.set("loaded");
+  return resp.data;
 };
 
 export const getTopStreamsOnPage = async (page) => {
-  try {
-    loading.set("loading");
-    const resp = await axios.get(`${apiEndpoint}/streams?page=${page}`);
-    loading.set("loaded");
-    return resp.data.streams;
-  } catch (err) {
-    console.log(err);
-  }
+  loading.set("loading");
+  const resp = await axios.get(`${apiEndpoint}/streams?page=${page}`);
+  loading.set("loaded");
+  return resp.data.streams;
 };
 
 export const getPages = async () => {
-  try {
-    const resp = await axios.get(`${apiEndpoint}/streams`);
+  const resp = await axios.get(`${apiEndpoint}/streams`);
 
-    const count = resp.data.queryInfo.qty;
-    const onPage = resp.data.queryInfo.onPage;
+  const count = resp.data.queryInfo.qty;
+  const onPage = resp.data.queryInfo.onPage;
 
-    if (count % onPage == 0) {
-      return count / onPage;
-    } else {
-      return Math.trunc(count / onPage + 1);
-    }
-  } catch (err) {
-    console.log(err);
+  if (count % onPage == 0) {
+    return count / onPage;
+  } else {
+    return Math.trunc(count / onPage + 1);
   }
 };
--- src/api/user.js
+++ src/api/user.js
@@ -5,12 +5,8 @@ import { apiEndpoint } from ".";
 import { loading } from "../stores/loading";
 
 export const getUser = async () => {
-  try {
-    loading.set("loading");
-    const resp = await axios.get(`${apiEndpoint}/user`);
-    loading.set("loaded");
-    return resp.data;
-  } catch (err) {
-    console.log(err);
-  }
+  loading.set("loading");
+  const resp = await axios.get(`${apiEndpoint}/user`);
+  loading.set("loaded");
+  return resp.data;
 };
